d2d413c238227b02db4d000e6fb47c00ec7ef934
Andrew Gerst <gerst20051@gmail.com>
2013-03-28 01:19:45 -0400

    connected to cleardb

--- quotes/mysql.config.php
+++ quotes/mysql.config.php
@@ -1,9 +1,9 @@
 <?php
-
-define('MYSQL_HOST','localhost');
-define('MYSQL_USER','');
-define('MYSQL_PASSWORD','');
-define('MYSQL_DATABASE','hnswave');
+$CLEARDB_URL = parse_url(getenv("CLEARDB_DATABASE_URL"));
+define('MYSQL_HOST',$CLEARDB_URL["host"]);
+define('MYSQL_USER',$CLEARDB_URL["user"]);
+define('MYSQL_PASSWORD',$CLEARDB_URL["pass"]);
+define('MYSQL_DATABASE',substr($CLEARDB_URL["path"],1));
 define('MYSQL_TABLE','quotes');
 define('MYSQL_TABLE_USERS','users');
 define('MYSQL_ALL_USERS','uid, email, username, firstname, lastname, access_level, last_login, date_joined, logins');
